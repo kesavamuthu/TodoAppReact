a4a8fc0eb687a6f550ed652d7c9478d5a1433568
kesava <kesava@localhost.localdomain>
2020-07-13 19:55:03 +0530

    Updates

---
+++ precache-manifest.3aa93a4ca5d7f37c0324dff3fd8bda0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4906fdff9e3ca98db19a966f838e4a2",
+    "url": "/TodoAppReact/index.html"
+  },
+  {
+    "revision": "1d4306e335976a889b01",
+    "url": "/TodoAppReact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3f171e115d8ea2c05d18",
+    "url": "/TodoAppReact/static/css/main.6e69a9dc.chunk.css"
+  },
+  {
+    "revision": "1d4306e335976a889b01",
+    "url": "/TodoAppReact/static/js/2.ac95263f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TodoAppReact/static/js/2.ac95263f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f171e115d8ea2c05d18",
+    "url": "/TodoAppReact/static/js/main.0d906f11.chunk.js"
+  },
+  {
+    "revision": "0177d83433d44c72f958",
+    "url": "/TodoAppReact/static/js/runtime-main.b6736c31.js"
+  }
+]);
